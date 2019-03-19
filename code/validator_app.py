3e4b616acd61d506d7b863ca242636b49138590d
mrychlicka <rychlickam@gmail.com>
2019-03-19 17:37:58 +0100

    removed redundant code

--- code/validator_app.py
+++ code/validator_app.py
@@ -1,10 +1,9 @@
 import os
-from flask import Flask, render_template, flash, request, redirect, url_for, send_from_directory
+from flask import Flask, render_template, flash, request, redirect
 from werkzeug.utils import secure_filename
 import validation
 
 
-UPLOAD_FOLDER = "/home/gosia/Downloads"
 UPLOAD_FOLDER = "%s" % (os.path.dirname(os.path.abspath(__file__)) + '/uploads')
 ALLOWED_EXTENSIONS = 'wav'
 
@@ -34,14 +33,8 @@ def upload_site():
 			sth_return = sth.split(" ")
 			is_valid = sth_return[0]
 			sample_number = " " if len(sth_return) == 1 else sth_return[1]
-
-			return redirect(url_for('validation_site', filename=filename, is_valid=is_valid, sample_number=sample_number))
+			return render_template('validation.html', filename=filename, is_valid=is_valid, sample_number=sample_number)
 	return render_template('upload_site.html')
-
-
-@validator_app.route('/validation/<filename>/<is_valid>/<sample_number>')
-def validation_site(filename, is_valid, sample_number):
-	return render_template('validation.html', filename=filename, is_valid=is_valid, sample_number=sample_number)
 
 
 if __name__ == '__main__':
